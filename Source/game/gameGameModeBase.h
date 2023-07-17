8f5579797301b03b0d5b319f90ed2b20b8f4ab67
trendyOP <114644820+trendyOP@users.noreply.github.com>
2023-07-18 01:47:41 +0900

    monster creature player

---
+++ Source/game/gameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_API AgameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
